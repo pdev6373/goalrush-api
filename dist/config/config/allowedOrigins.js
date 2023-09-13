319d91a76fa3429fad2621f288b89c0e57497adc
pdev6373 <pdev6373@gmail.com>
2023-09-13 01:54:07 +0100

    working on the livesccore api

---
+++ dist/config/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.allowedOrigins = void 0;
+exports.allowedOrigins = [
+    "http://localhost:3000",
+    "https://wwww.goalrush.com",
+    "https://goalrush.com",
+];
---
+++ types.ts
@@ -0,0 +1,29 @@
+type LiveScoreDetailsType = {
+  stageId: string;
+  stageName: string;
+  stageRoute: string;
+  competitionName: string;
+  competitionRoute: string;
+  time: string;
+};
+
+type LiveScoreTeamDetailsType = {
+  id: string;
+  name: string;
+  score: number;
+  logo: string;
+};
+
+type LiveScoreEventType = {
+  eventId: string;
+  isLive: boolean;
+  winningTeam: "home" | "away" | "none";
+  time: string;
+  homeTeam: LiveScoreTeamDetailsType;
+  awayTeam: LiveScoreTeamDetailsType;
+};
+
+export type AllLiveScoresType = {
+  details: LiveScoreDetailsType;
+  events: LiveScoreEventType[];
+};
